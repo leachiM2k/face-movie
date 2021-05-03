a778b2e504c2e17e2e6c048bb4efb0643e04408f
Michael Rotmanov <leachiM2k@leachiM2k.de>
2021-05-03 15:12:16 +0200

    do not let ffmpeg fail on odd image sizes (thx to @net-attack)
    closes #2

--- start.sh
+++ start.sh
@@ -13,6 +13,6 @@ convert payload/output/c_* -delay 1 -morph 1 morph/%05d.morph.jpg
 echo "++++++++++++++ Combining to movie"
 
 rm -f payload/out_morphed.mp4
-ffmpeg -framerate 10 -pattern_type glob -i 'morph/*.jpg' -c:v libx264 -pix_fmt yuv420p payload/out_morphed.mp4
+ffmpeg -framerate 10 -pattern_type glob -i 'morph/*.jpg' -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -pix_fmt yuv420p payload/out_morphed.mp4
 
 echo "++++++++++++++ Facemovie is ready! (out_morphed.mp4)"
