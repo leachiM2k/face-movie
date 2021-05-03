8ae16ab8595a0460ecb0b2c7cfc27afc431e87aa
Michael Rotmanov <leachiM2k@leachiM2k.de>
2021-05-03 15:11:01 +0200

    do not let ffmpeg fail on odd image sizes (thx to @net-attack)
    closes #2

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python main.py
-ffmpeg -framerate 10 -pattern_type glob -i 'output/face_*.png' -c:v libx264 -pix_fmt yuv420p out.mp4
+ffmpeg -framerate 10 -pattern_type glob -i 'output/face_*.png' -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -pix_fmt yuv420p out.mp4
 
 echo "Facemovie is ready! (out.mp4)"
